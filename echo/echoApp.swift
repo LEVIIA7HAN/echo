74e2c14100375193d05e5bca8daa5923444695d1
Howard C <45508175+LEVIIA7HAN@users.noreply.github.com>
2021-03-10 05:05:24 -0800

    Initial Commit

---
+++ echo/echoApp.swift
@@ -0,0 +1,17 @@
+//
+//  echoApp.swift
+//  echo
+//
+//  Created by Howard(temp) on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct echoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
